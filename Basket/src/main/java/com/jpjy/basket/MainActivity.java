2efebe2da782ab01bd58627e3a129e2c1a6d66df
Bo Gou <link@gps-e.com>
2013-11-29 22:51:10 +0800

    update

--- Basket/src/main/java/com/jpjy/basket/MainActivity.java
+++ Basket/src/main/java/com/jpjy/basket/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends Activity {
 
         try {
             String ret = readFile("data.xml");
-            if (ret != null)
+            if (!ret.equals(""))
                 mData = domService.getDataResult(ret);
         } catch (Exception e) {
             e.printStackTrace();
